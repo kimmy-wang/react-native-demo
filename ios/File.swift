2127b157302c1c05624a101fd07789b65c14151e
upcwangying <upcwangying@126.com>
2020-03-13 17:00:48 +0800

    feat: lottie supported.

---
+++ ios/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  ReactNativeGalleryDemo
+//
+//  Created by Ying Wang on 2020/3/13.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
--- src/pages/WebView/index.js
+++ src/pages/WebView/index.js
@@ -1,7 +1,20 @@
 import React, {useState, useEffect, useCallback, useRef} from 'react';
-import {SafeAreaView, StyleSheet, Platform, BackHandler} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  Platform,
+  StatusBar,
+  BackHandler,
+  Dimensions,
+} from 'react-native';
 
 import {WebView} from 'react-native-webview';
+import LottieView from 'lottie-react-native';
+import {getStatusBarHeight} from 'react-native-status-bar-height';
+
+const STATUSBAR_HEIGHT =
+  Platform.OS === 'ios' ? getStatusBarHeight(true) : StatusBar.currentHeight;
 
 const CWebView = () => {
   const webViewRef = useRef(null);
@@ -36,6 +49,19 @@ const CWebView = () => {
     setGoBack(navState.canGoBack);
   };
 
+  const renderLoading = () => {
+    return (
+      <View style={styles.webViewLoadingContainer}>
+        <LottieView
+          source={require('../../assets/animations/loading.json')}
+          style={styles.loading}
+          autoPlay
+          loop
+        />
+      </View>
+    );
+  };
+
   return (
     <>
       <SafeAreaView style={styles.container}>
@@ -44,19 +70,36 @@ const CWebView = () => {
           originWhitelist={['*']}
           source={{uri: 'https://reactnative.cn'}}
           useWebKit={true}
+          startInLoadingState={true}
           onNavigationStateChange={onNavigationStateChange}
+          renderLoading={renderLoading}
         />
       </SafeAreaView>
     </>
   );
 };
 
+const LOADING_SIZE = 80;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  scrollView: {
-    backgroundColor: '#F3F3F3',
+  webViewLoadingContainer: {
+    position: 'absolute',
+    top:
+      Dimensions.get('window').height * 0.5 -
+      STATUSBAR_HEIGHT -
+      LOADING_SIZE / 2 -
+      20,
+    left: Dimensions.get('window').width * 0.5 - LOADING_SIZE / 2,
+    width: LOADING_SIZE,
+    height: LOADING_SIZE,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loading: {
+    width: LOADING_SIZE,
+    height: LOADING_SIZE,
   },
 });
 
